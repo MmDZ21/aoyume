de9793270a0dae768cbfdf9cb91d04543bf26297
MmDZ21 <mmohamadzz294@gmail.comgit config --global user.name MmDZ21git config --global user.email mmohamadzz294@gmail.com>
2025-11-25 19:54:14 +0330

    implement eslint & prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,32 @@
 import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
-
+import tailwind from "eslint-plugin-tailwindcss";
+import prettier from "eslint-config-prettier";
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
+  ...tailwind.configs["flat/recommended"],
+  {
+    settings: {
+      tailwindcss: {
+        callees: ["cn", "cva"],
+        cssFiles: [
+          "**/*.css",
+          "!**/node_modules",
+          "!**/.*",
+          "!**/dist",
+          "!**/build",
+        ],
+        removeDuplicates: true,
+        skipClassAttribute: false,
+        whitelist: ["bg-foreground", "text-background"],
+        tags: [],
+        classRegex: "^class(Name)?$",
+      },
+    },
+  },
+  prettier,
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
